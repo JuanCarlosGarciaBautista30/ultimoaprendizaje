944155beb4436237d36f4ce403ed2aac42cf0420
Eduardo G <proyectosngegb@gmail.com>
2023-08-03 21:57:56 -0600

    Create a prgram that tellme if a number is pair

--- provando.py
+++ provando.py
@@ -1,2 +1,11 @@
 #Todo esto es un ejercicio para p[robar git hub desde nuestras casas
 print("hola ésta es un alinea de codigo en python")
+
+
+#generame un programa que pida un numero entero y diga si es par o impar
+numero = int(input("Dime un numero: ")) 
+
+if numero % 2 == 0: 
+    print("Es par")
+else:       
+    print("Es impar");
---
+++ test2.py
@@ -0,0 +1,8 @@
+#generame un programa d epython que lea un numero entero y diga si es par o impar
+
+numero = int(input("Dime un numero: "))
+
+if numero % 2 == 0:
+    print("Es par")
+else:    
+    print("Es impar")
